dede3a1e5bea47bd2c286a271ad9fd696456077d
nihal-soni <nihalsoni0523@gmail.com>
2025-09-17 01:43:17 +0530

    feat(introduction): add Introduction section with Tag component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { Hero } from "@/sections/Hero";
+import Introduction from "@/sections/introduction";
+
+import LogoTicker from "@/sections/logoticker";
 import { Navbar } from "@/sections/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <LogoTicker />
+      <Introduction />
     </div>
   );
 }
